0732051f651c0d0e0f0d0f5e1de7b6e1d0db5c34
adeola2310 <adekoyejoadeola@gmail.com>
2020-10-26 09:14:54 +0100

    dahboard structure

---
+++ src/pages/Dashboard/Dashboard.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./Dashboard.scss";
+const Dashboard = () => {
+    return (
+        <div className="app">
+            <header className="header">
+                <div className="header__logo">
+                    {/*<img src={require('')} alt="logo"/>*/}
+                    <h3>Shopify</h3>
+                </div>
+
+                <div className="header__notify">
+
+                </div>
+            </header>
+            <aside>
+
+            </aside>
+            <main>
+
+            </main>
+        </div>
+    )
+}
+
+export default  Dashboard;
